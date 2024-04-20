bc6f6ec283181d8e030dc489872495f4a8784840
引山裕貴 <hikkybass@gmail.com>
2024-04-20 12:02:20 +0900

    コメント追加

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+// コメント追加
+
 export interface Env {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
 	// MY_KV_NAMESPACE: KVNamespace;
